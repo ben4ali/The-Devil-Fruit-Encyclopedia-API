4f7e3f8179cfca40e6eb1652c6dbd47ca111d586
Ali <alibenkarrouch@gmail.com>
2025-04-09 18:49:46 -0400

    Initial commit

---
+++ src/main/java/com/devilfruitencyclopedia/thedevilfruitencyclopediaapi/TheDevilFruitEncyclopediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.devilfruitencyclopedia.thedevilfruitencyclopediaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDevilFruitEncyclopediaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheDevilFruitEncyclopediaApiApplication.class, args);
+    }
+
+}
